a04675ee0b50231988e60e95fe0b7b06cae5d260
Salman <sallu123xx@gmail.com>
2025-09-01 08:48:58 +0530

    test: integration tests for account deletion

--- backend/src/__tests__/int/admin.spec.ts
+++ backend/src/__tests__/int/admin.spec.ts
@@ -130,5 +130,56 @@ describe("Admin", () => {
       expect(res.statusCode).toBe(STATUS_CODES.OK);
       expect(res.body).toBeDefined();
     });
+
+    test("Roles other than 'SUPERADMIN' cannot delete admin account", async () => {
+      const mockAdmin1 = {
+        name: "Test",
+        password: "test123",
+      };
+      const mockAdmin2 = {
+        name: "Test2",
+        password: "test1234",
+      };
+
+      const superAdminRes = await request(app)
+        .post("/api/admin/auth/login")
+        .send(superadmin);
+      const newAdminRes1 = await request(app)
+        .post("/api/admin")
+        .set("Authorization", `Bearer ${superAdminRes.body.accessToken}`)
+        .send(mockAdmin1);
+      const newAdminRes2 = await request(app)
+        .post("/api/admin")
+        .set("Authorization", `Bearer ${superAdminRes.body.accessToken}`)
+        .send(mockAdmin2);
+
+      const res = await request(app)
+        .delete(`/api/admin/${newAdminRes2.body.admin.id}`)
+        .set("Authorization", `Bearer ${newAdminRes1.body.accessToken}`);
+      expect(res.statusCode).toBe(STATUS_CODES.FORBIDDEN);
+      expect(res.body.message).toBe("Unauthorized");
+    });
+
+    test("'SUPERADMIN' can delete admin account", async () => {
+      const mockAdmin = {
+        name: "Test",
+        password: "test123",
+      };
+
+      const superAdminRes = await request(app)
+        .post("/api/admin/auth/login")
+        .send(superadmin);
+      const newAdminRes = await request(app)
+        .post("/api/admin")
+        .set("Authorization", `Bearer ${superAdminRes.body.accessToken}`)
+        .send(mockAdmin);
+
+      const res = await request(app)
+        .delete(`/api/admin/${newAdminRes.body.admin.id}`)
+        .set("Authorization", `Bearer ${superAdminRes.body.accessToken}`);
+      expect(res.statusCode).toBe(STATUS_CODES.OK);
+      expect(res.body.result).toBeDefined();
+      expect(res.body.result.id).toBeDefined();
+    });
   });
 });
