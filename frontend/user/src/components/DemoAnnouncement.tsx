6eb84056f9534fc53c7e2507511973a0feaa0990
Salman <sallu123xx@gmail.com>
2025-09-08 13:53:46 +0530

    build: modify demo announcement

--- frontend/user/src/components/DemoAnnouncement.tsx
+++ frontend/user/src/components/DemoAnnouncement.tsx
@@ -6,11 +6,24 @@ const DemoAnnouncement = () => {
 
   return (
     <div
-      className={`${open ? "flex" : "hidden"} w-full items-center justify-center gap-3 bg-blue-500 px-12 py-6 text-white md:text-xl`}
+      className={`${open ? "flex" : "hidden"} w-full items-center justify-center gap-3 bg-orange-100 px-12 py-6 text-orange-800 md:text-xl`}
     >
       <p className="md:ml-auto">
-        This is a demo version. The servers would only be available between{" "}
-        <span className="underline">13:00 - 21:00 IST</span>.
+        {/* This is a demo version. The servers would only be available between{" "} */}
+        {/* <span className="underline">13:00 - 21:00 IST</span>. */}
+        {/* This project is currently hosted on a free-tier server, which may occasionally result in slower response times or brief loading delays because of cold starts, typically 30-90 seconds. */}
+        <p className="flex items-center justify-center gap-3">
+          <div className="size-3 rounded-full bg-orange-500"></div>
+          <span className="font-bold">SERVER NOTICE</span>
+          <div className="size-3 rounded-full bg-orange-500"></div>
+        </p>
+        <p>
+          This project is hosted on a free-tier server, which may result in
+          initial loading delays, typically between{" "}
+          <span className="font-semibold underline">30 to 90 seconds,</span>{" "}
+          especially after periods of inactivity (cold starts).
+        </p>
+        <p>Thank you for your patience and understanding!</p>
       </p>
       <IoClose
         onClick={() => setOpen(false)}
