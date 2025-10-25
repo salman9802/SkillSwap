cd4401d96463641be26cfc78c7a82d2682e8907b
Salman <sallu123xx@gmail.com>
2025-10-25 10:30:06 +0530

    chore: update server url env var

--- frontend/admin/src/api/client.ts
+++ frontend/admin/src/api/client.ts
@@ -1,13 +1,17 @@
 import axios from "axios";
 import { attahTokenInterceptors } from "./interceptors";
 
+const baseURL = import.meta.env.PROD
+  ? import.meta.env.VITE_ADMIN_API_URL
+  : "http://localhost";
+
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_ADMIN_API_URL,
+  baseURL,
   withCredentials: true,
 });
 
 export const privateApi = axios.create({
-  baseURL: import.meta.env.VITE_ADMIN_API_URL,
+  baseURL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
